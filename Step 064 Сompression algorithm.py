0c545e9a6f5c32864292589beebd569561b1eb77
Maria Korobeynikova <siverka@gmail.com>
2017-03-12 19:27:48 +0300

    clean up step 064

--- Step 064 Сompression algorithm.py
+++ Step 064 Сompression algorithm.py
@@ -1,10 +1,7 @@
 import re
 
-# string = input()
-string = 'aaaabbcaa'
+string = input()
+# string = 'aaaabbcaa'
 groups = re.findall(r'((\w)\2*)', string)
 result = [group[1] + str(len(group[0])) for group in groups]
-# print(''.join(result))
-
-re.sub(r'((\w)\2*)', r'\1', string)
-print(string)
+print(''.join(result))
