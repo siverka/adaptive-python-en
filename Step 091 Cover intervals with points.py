f4409e1c31244c75623f2192f7e5e264bac3ea81
Maria Korobeynikova <siverka@gmail.com>
2017-03-15 21:44:33 +0300

    resolved step 091 Cover intervals with points

--- Step 091 Cover intervals with points.py
+++ Step 091 Cover intervals with points.py
@@ -1,20 +1,23 @@
-# n = int(input())
-# intervals = [[int(i) for i in input().split()] for _ in range(n)]
-
-
-def end_point(interval):
-    return interval[1]
-
-
-def is_intersects(interval1, interval2):
+def is_intersected(interval1, interval2):
     return max(interval1[0], interval2[0]) <= min(interval1[1], interval2[1])
 
 
-n = 4
-intervals = [[4, 7], [1, 3], [2, 5], [5, 6]]
+def points(intervals):
+    intervals = sorted(intervals, key=lambda interval: interval[1])
+    result_set = []
 
-intervals = sorted(intervals, key=end_point)
-print(intervals)
+    while intervals:
+        interval = intervals.pop(0)
+        result_set.append(interval)
+        intervals = [x for x in intervals if not is_intersected(x, interval)]
 
-result_set = []
-# http://cs.stackexchange.com/questions/43197/relation-between-the-point-cover-interval-problem-and-the-interval-scheduling
+    print(len(result_set))
+    for interval in result_set:
+        print(interval[1], end=" ")
+
+
+n = int(input())
+intervals = [[int(i) for i in input().split()] for _ in range(n)]
+points(intervals)
+# points([[4, 7], [1, 3], [2, 5], [5, 6], [2, 5], [4, 7]])  # 2, 3 6
+# points([[1, 3], [2, 5], [3, 6]])  # 1, 3
