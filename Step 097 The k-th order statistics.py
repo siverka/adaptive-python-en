4b96ff1e75fb1ef67227330ab75a298a73b98d6d
Maria Korobeynikova <siverka@gmail.com>
2017-03-17 23:20:38 +0300

    cleaned up step 097 The k-th order statistics

--- Step 097 The k-th order statistics.py
+++ Step 097 The k-th order statistics.py
@@ -1,41 +1,31 @@
 def partition(list, k, n):
     reference = list[-1]
     i, j = 0, 0
-    print('k', k, 'list: ', list)
     while j < n - 1:
         if list[j] < reference:
-            a = list[i]
-            list[i] = list[j]
-            list[j] = a
+            list[i], list[j] = list[j], list[i]
             i += 1
             j += 1
         else:
             j += 1
-    a = list[i]
-    list[i] = list[j]
-    list[j] = a
+    list[i], list[j] = list[j], list[i]
 
-    print('k:', k, 'list:', list)
-    print('reference:', reference, 'list:', list[:i], k)
-    print('reference:', reference, 'list:', list[i:], k - i)
-    print('i:', i, ',j:', j, ',k:', k)
-    print('------------')
     if i == k:
         return list[i]
     if i == 0:
         return partition(list[1:], k - 1, n - 1)
-    if i == len(list) - 1:
-        return partition(list[:-1], k, n - 1)
-    if k < i:
+    if k < i or i == n - 1:
         return partition(list[:i], k, i)
     else:
         return partition(list[i:], k - i, n - i)
 
 
-# array = [3, 6, 5, 7, 2, 9, 8, 10, 4, 1]
-# print(partition(array, 9, 10))
-
 n, k = (int(i) for i in input().split())
-list = [int(i) for i in input().split()]
-print(partition(list, k, n))
+array = [int(i) for i in input().split()]
+print(partition(array, k, n))
+
+# array = [3, 6, 5, 7, 2, 9, 8, 10, 4, 1]
+# print(partition(array, 0, 10))  # 1
+# print(partition(array, 9, 10))  # 10
+# print(partition(array, 4, 10))  # 5
 
