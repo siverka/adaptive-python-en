b439d326d1d3f96cab8321cfb44822a0b2edf0d5
Maria Korobeynikova <siverka@gmail.com>
2017-03-20 00:44:46 +0300

    resolved step 100 Calculator

--- Step 100 Calculator.py
+++ Step 100 Calculator.py
@@ -1,16 +1,9 @@
-# n = int(input())
-n = 96234
-# n = 5
-# n = 3
-# n = 2
-# n = 1
+n = int(input())
 f = [0, 1, 1]  # number of steps for [1, 2, 3]
-g = [1, 2]
-
+g = [-1, 1, 2]
 
 for m in range(4, n + 1):  # number
     i = m - 1  # index
-    # print('m', m, 'i', i)
     steps = [f[i-1] + 1]
     op = [0]
     if m % 2 == 0:
@@ -19,34 +12,18 @@ for m in range(4, n + 1):  # number
     if m % 3 == 0:
         steps.append(f[m // 3 - 1] + 1)
         op.append(2)
-    # print('steps', steps)
-    # print('op', op)
     f.append(min(steps))
     g.append(op[steps.index(f[-1])])
 
-# print('results')
-# print(f)
-# print('num of steps:', f[-1])
-# print(g)
-# print('----------------')
-
 operations = [lambda x: x - 1, lambda x: x // 2, lambda x: x // 3]
 inter = []
-k = n - 2
 m = n
-while m >= 1:
-    # print('n', n, 'k', k)
+while True:
     inter.append(m)
-    m = operations[g[k]](m)
-    k = operations[g[k]](k)
-    # print(inter)
-    # print('op:', g[k])
-    # print('new n', n)
+    op = g[m-1]
+    if op == -1:
+        break
+    m = operations[op](m)
 
-# if n == 1:
-#     print(0)
-# else:
-#     print(f[-1])
-# print(*inter[::-1])
-print(f[-1])
-print(len(inter))
+print(f[n - 1])
+print(*inter[::-1])
