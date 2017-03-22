43468497fcab42768f99a5d4c3597948860be90b
Maria Korobeynikova <siverka@gmail.com>
2017-03-22 21:48:58 +0300

    resolved step 124 Almost representative of the majority

--- Step 124 Almost representative of the majority.py
+++ Step 124 Almost representative of the majority.py
@@ -1,5 +1,7 @@
 n = int(input())
-array = [int(i) for i in input().split()]
-repeats = [array.count(a) for a in set(array)]
-answer = any(map(lambda r: r > n / 3, repeats))
+array = input().split()
+repeats = {a: 0 for a in set(array)}
+for a in array:
+    repeats[a] += 1
+answer = any(map(lambda r: r > n / 3, repeats.values()))
 print(int(answer))
