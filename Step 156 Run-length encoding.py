ef77de6db51b5389bc48bb5504d5a0f0f7b3a39f
Maria Korobeynikova <siverka@gmail.com>
2017-04-04 20:52:37 +0300

    resolved step 156 Run-length encoding

--- Step 156 Run-length encoding.py
+++ Step 156 Run-length encoding.py
@@ -1,2 +1,8 @@
-# string = input()
-string = 'aaabccccCCaB'
+from re import sub
+
+def encode(matchobj):
+    return str(len(matchobj.group(0))) + matchobj.group(0)[0]
+
+string = input()
+string = sub(r'(\w)\1+', encode, string)
+print(string)
