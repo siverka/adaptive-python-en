b37ad850e4fca30516b645e68246f6a5787226da
Maria Korobeynikova <siverka@gmail.com>
2017-05-02 17:54:37 +0300

    continued step 246 The most frequent word

--- Step 246 The most frequent word.py
+++ Step 246 The most frequent word.py
@@ -1,8 +1,13 @@
-words = []
+from operator import itemgetter
 
-with open('file', 'r') as file:
-    for line in file:
-        words.append([w.lower() for w in line.strip()])
+words = [w.lower() for w in input().split()]
+# words = []
+# with open('file', 'r') as file:
+#     for line in file:
+#         words.append([w.lower() for w in line.strip()])
 
 set_words = list(set(words))
 counts = [words.count(w) for w in set_words]
+d = list(zip(set_words, counts))
+d.sort(key=itemgetter(1, 0))
+print(d)
